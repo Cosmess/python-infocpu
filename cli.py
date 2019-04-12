ef6f6679081ed48127a0d995d2f61af1b9ea4db0
Cosme Sousa <36869385+Cosmess@users.noreply.github.com>
2019-04-12 02:28:54 -0300

    Add files via upload

---
+++ cli.py
@@ -0,0 +1,14 @@
+from program import computer , cpu, memory
+
+import sys
+
+print("======================================================================")
+print( "Sistema Operacional:",computer.distro(),computer.osVersion())
+print("Tipo de Processador:",computer.cpu())
+print("Arquitetura:",computer.arc())
+print("Velocidade:", cpu.freq(),"GHz")
+print("Nucleos:", cpu.cores())
+print("Nucleos Fisicos:",cpu.phycores())
+print("Memoria:",memory.size(),"Gb")
+print("Nome Do Computador:",computer.name())
+print("======================================================================")
